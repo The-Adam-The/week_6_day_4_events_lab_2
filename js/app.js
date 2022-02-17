331f84428be9b31c78112660f90e79fc2438e369
Leszek (Les) <leszek.modrzejewski@gmail.com>
2022-02-17 15:26:14 +0000

    first title list element added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const handleForm = function(event) {
+    event.preventDefault()
+    console.log('the form event listener is working');
+    // console.log(event.target.title.value);
+    // console.log(event.target.author.value);
+    // console.log(event.target.category.value);
+
+    const titleListItem = document.createElement('li')
+    titleListItem.textContent = `${event.target.title.value}`
+
+    const list = document.querySelector('#reading-list')
+    list.appendChild(titleListItem)
+
+  }
+
+  const form = document.querySelector('#new-item-form')
+  form.addEventListener('submit', handleForm)
 })
